5445a1424c44ec489166bffd29d560e854eb1a9a
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-01 16:44:30 -0700

    learned how to bind in vue

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        title: "Hello World!"
+        title: "Hello World",
+        name: "Aljan", 
+        url: "https://www.facebook.com",
+        classes:['one', 'two']
+    }, 
+    methods: {
+        greet(time) {
+            return `Hello and good${time}, ${this.name}.`
+
+        }
     }
 })
