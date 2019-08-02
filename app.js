370dc3e0d80c58590498abbd9246264078aaa93c
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-01 18:12:35 -0700

    Set up Vue as NPM

--- app.js
+++ app.js
@@ -2,30 +2,7 @@ new Vue({
     el: '#app',
     data: {
         title: "Vue Ninja",
-        items: [
-            "Apple",
-            "Orange",
-            "Banana",
-            "Strawberry",
-            "Pineapple"
-        ],
-        ninjas: [{
-            name: "Crystal",
-            age: 25,
-            belt: "black"
-        }, {
-            name: "Ken",
-            age: 19,
-            belt: "green"
-        }, {
-            name: "Ryu",
-            age: 21,
-            belt: "White"
-        }, {
-            name: "Paul",
-            age: 23,
-            belt: "orange"
-        }]
+
     },
     methods: {
 
