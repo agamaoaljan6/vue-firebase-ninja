98274dc5280ab82cb048526fed2acb34aa700deb
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-01 21:48:34 -0700

    removed mentors files added mentees file learned about lifecycle hooks beforecreate created beforeupdate updated

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -3,42 +3,27 @@
   <div id="app">
     <h1>{{title}}</h1>
     <Navbar />
-    <AllMentors :mentors="mentors" @delete="deleteMentor" />
-    <AvailableMentors :mentors="mentors" />
+    <Mentees/>
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
-import AllMentors from "./AllMentors";
-import AvailableMentors from "./AvailableMentors";
+import Mentees from './Mentees'
 export default {
   name: "app",
   components: {
     Navbar,
-    AvailableMentors,
-    AllMentors
+    Mentees
   },
   data() {
     return {
       title: "My first Vue App",
-      mentors: [
-        // I can fetch the mentors that i set up in firebase database in here
-        { name: "Aljan", email: "agamaoaljan@gmail.com", availability: true },
-        { name: "Jon", email: "jonsnow@gmail.com", availability: false },
-        { name: "Mike", email: "mikemiller@gmail.com", availability: true },
-        { name: "John", email: "johnsmith@gmail.com", availability: false }
-      ]
+
     };
   },
   methods: {
-    deleteMentor(payload) {
-      // naming convention payload
-      // console.log(payload)
-      this.mentors = this.mentors.filter(mentor => {
-        return mentor.name !== payload.name
-      })
-    }
+  
   }
 };
 </script>
---
+++ myproject/src/Mentees.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mentees">
+        <h2>{{menteeName}}</h2>
+        <button @click="changeTitle">Change Title</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'mentees',
+    data() {
+        return {
+            menteeName: "Aljan"
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.menteeName = "Nash"
+        }
+    },
+
+    beforeCreate() {
+        alert('beforeCreate hook')
+    }, 
+    created() {
+        alert('created hook')
+    }, 
+    beforeUpdate() {
+        alert('beforeUpdate hook')
+    },
+    updated() {
+        alert('updated hook')
+    }
+}
+</script>
