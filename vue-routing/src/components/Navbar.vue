a13918963d2576a8af7dd4e9d8987647ea83e898
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-02 10:04:01 -0700

    router links on vue

---
+++ vue-routing/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li>
+        <router-link to="/">HOME</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'About'}">ABOUT</router-link>
+      </li>
+      <!-- :to="{name: 'About'}" 
+            will be calling the name of the router About
+      it's also the same as the to="/about"-->
+    </ul>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  name: "Navbar",
+  data() {},
+  methods: {}
+};
+</script>
+
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+a {
+  color: #42b983;
+}
+</style>
