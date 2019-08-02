cd2a7da94653975381364e878dee74f6c9dbb1b7
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-01 21:19:44 -0700

    custom events specifically delete onclick event from child to parent

--- myproject/src/AllMentors.vue
+++ myproject/src/AllMentors.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="all-mentors">
     <h2>All Mentors</h2>
-    <div v-for="(mentor,index) in mentors" :key="index"> 
-        <!--the :key is the same as v-bind:key="" -->
-        <!--if you are looping through array, every item must be unique, thus needing the key -->
-      <span>{{mentor.name}}</span>
+    <div v-for="(mentor,index) in mentors" :key="index">
+      <!--the :key is the same as v-bind:key="" -->
+      <!--if you are looping through array, every item must be unique, thus needing the key -->
+      <span>
+        {{mentor.name}}
+        <button @click="endMentorship(mentor.name)">Delete</button>
+      </span>
     </div>
   </div>
 </template>
@@ -13,11 +16,17 @@
 <script>
 export default {
   name: "AllMentors",
-  props: ['mentors'],
+    props: ["mentors"],
   data() {
-    return {
-
-    };
+    return {};
+  },
+  methods: {
+    endMentorship(name) {
+      this.$emit("delete", { name });
+      //.$emit = method that can use to emit custom event from this component
+      // it has 2 parametes, name of the event(any name you want) and the data
+      // you want to send inside the object {}
+    }
   }
 };
 </script>
--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -3,7 +3,7 @@
   <div id="app">
     <h1>{{title}}</h1>
     <Navbar />
-    <AllMentors :mentors="mentors" />
+    <AllMentors :mentors="mentors" @delete="deleteMentor" />
     <AvailableMentors :mentors="mentors" />
   </div>
 </template>
@@ -23,12 +23,22 @@ export default {
     return {
       title: "My first Vue App",
       mentors: [
-        { name: "Aljan", email: "agamaoaljan@gmail.com", availability:true },
-        { name: "Jon", email: "jonsnow@gmail.com", availability:false },
-        { name: "Mike", email: "mikemiller@gmail.com", availability:true },
-        { name: "John", email: "johnsmith@gmail.com", availability:false }
+        // I can fetch the mentors that i set up in firebase database in here
+        { name: "Aljan", email: "agamaoaljan@gmail.com", availability: true },
+        { name: "Jon", email: "jonsnow@gmail.com", availability: false },
+        { name: "Mike", email: "mikemiller@gmail.com", availability: true },
+        { name: "John", email: "johnsmith@gmail.com", availability: false }
       ]
     };
+  },
+  methods: {
+    deleteMentor(payload) {
+      // naming convention payload
+      // console.log(payload)
+      this.mentors = this.mentors.filter(mentor => {
+        return mentor.name !== payload.name
+      })
+    }
   }
 };
 </script>
